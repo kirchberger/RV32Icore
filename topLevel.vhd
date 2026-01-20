41a1ffdd3f44b6b338567ba447cfd1b724a28684
kirchberger <kirchbergeralex0@gmail.com>
2026-01-19 23:04:34 -0500

    updated

--- topLevel.vhd
+++ topLevel.vhd
@@ -6,7 +6,6 @@ use IEEE.numeric_std.All;
 
 entity topLevel is
     Port ( dataOut : inout STD_LOGIC_VECTOR (31 downto 0);
-				wr : inout STD_LOGIC;
            clk : in  STD_LOGIC;
            enable : in  STD_LOGIC);
 end topLevel;
@@ -20,7 +19,8 @@ signal pc : STD_LOGIC_VECTOR (31 downto 0);
 signal pcNext : STD_LOGIC_VECTOR (31 downto 0);
 signal immExtend : STD_LOGIC_VECTOR (31 downto 0);
 signal aluResult : STD_LOGIC_VECTOR (31 downto 0);
-
+signal wr1 : STD_LOGIC;
+signal wr2 : STD_LOGIC;
 begin
 
 
@@ -31,7 +31,8 @@ begin
 			pc,
 			clk,
 			enable);
-			
+	
+	-- Should change to raw logic to synthsise better
 	INSTANCE_PCINC : entity work.pcIncrementor
 		port map (
 			pc,
@@ -49,7 +50,11 @@ begin
 			instruction(19 downto 15),
 			instruction(24 downto 20),
 			rd1,
-			rd2);
+			rd2,
+			dataOut,
+			instruction (11 downto 7),
+			wr2,
+			clk);
 	
 	-- Should be updated to some sort of memory controller
 	INSTANCE_DATAMEM : entity work.dataMemory
@@ -57,7 +62,7 @@ begin
 			rd2,
          dataOut,
          aluResult,
-         wr,
+         wr1,
          clk);
 	
 	-- Dumb extender that just extends for the store instruction
@@ -67,6 +72,7 @@ begin
 			immExtend);
 			
 	-- Dumb ALU right now that just adds
+	-- should change to raw logic that synthesises better
 	INSTANCE_ALU : entity work.ALU
 		port map (
 			rd1,
@@ -75,7 +81,9 @@ begin
 			
 			
 	-- Signal to write to the data memory, signals like such will be added to a seperate file later
-	wr <= instruction(0) and instruction(1) and (not instruction(2)) and (not instruction(3)) and (not instruction(4)) and instruction(5) and (not instruction(6));
+	wr1 <= instruction(0) and instruction(1) and (not instruction(2)) and (not instruction(3)) and (not instruction(4)) and instruction(5) and (not instruction(6));
 	
+	-- Signal to write to the registers, signals like such will be added to a seperate file later
+	wr2 <= instruction(0) and instruction(1) and (not instruction(2)) and (not instruction(3)) and (not instruction(4)) and (not instruction(5)) and (not instruction(6));
 end Behavioral;
 
